ad860bb0af91bb5cd497bacff552713e377470b3
damjanh <damjan.hudobreznik@gmail.com>
2020-06-29 12:40:59 +0200

    Managing some complex state.

--- src/index.js
+++ src/index.js
@@ -2,41 +2,85 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-const App = ({initialCount, initialText}) => {
-  const [count, setCount] = useState(initialCount);
-  const [text, setText] = useState(initialText);
+const NoteApp = () => {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
 
-  const increment = () => {
-    setCount(count + 1);
+  const addNote = (e) => {
+    e.preventDefault();
+    setNotes([
+      ...notes,
+      { title, body }
+    ]);
+    setTitle('');
+    setBody('');
   };
 
-  const decrement = () => {
-    setCount(count -1);
-  }
-
-  const reset = () => {
-    setCount(initialCount);
+  const removeNote = (title) => {
+    setNotes(
+      notes.filter((note) => note.title !== title)
+    );
   };
 
   return (
     <div>
-      <p>The current {text} is {count}.</p>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input type="text" value={text} onChange={(e)=> setText(e.target.value)}></input>
+      <h1>Notes</h1>
+      {notes.map((note) => (
+          <div key={note.title}>
+            <h3>{note.title}</h3>
+            <p>{note.body}</p>
+            <button onClick={() => removeNote(note.title)}>x</button>
+          </div>
+        )
+      )}
+      <p>
+        Add Note
+      </p>
+      <form onSubmit={addNote}>
+        <input type="text" value={title} onChange={(e) => { setTitle(e.target.value) }}/>
+        <textarea value={body} onChange={(e) => { setBody(e.target.value)}}></textarea>
+        <button>Add Note</button>
+      </form>
     </div>
   );
 }
 
-App.defaultProps = {
-  initialCount: 0,
-  initialText: 'count',
-};
+// const App = ({initialCount, initialText}) => {
+//   const [count, setCount] = useState(initialCount);
+//   const [text, setText] = useState(initialText);
+
+//   const increment = () => {
+//     setCount(count + 1);
+//   };
+
+//   const decrement = () => {
+//     setCount(count -1);
+//   }
+
+//   const reset = () => {
+//     setCount(initialCount);
+//   };
+
+//   return (
+//     <div>
+//       <p>The current {text} is {count}.</p>
+//       <button onClick={increment}>+1</button>
+//       <button onClick={decrement}>-1</button>
+//       <button onClick={reset}>Reset</button>
+//       <input type="text" value={text} onChange={(e)=> setText(e.target.value)}></input>
+//     </div>
+//   );
+// }
+
+// App.defaultProps = {
+//   initialCount: 0,
+//   initialText: 'count',
+// };
 
 ReactDOM.render(
   <React.StrictMode>
-    <App initialCount={10}/>
+    <NoteApp/>
   </React.StrictMode>,
   document.getElementById('root')
 );
