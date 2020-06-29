d16647feb7d0f8f7aaa835f58f507ce93de34e9d
damjanh <damjan.hudobreznik@gmail.com>
2020-06-29 11:06:28 +0200

    Expand on the useState hook.

--- src/index.js
+++ src/index.js
@@ -2,24 +2,38 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = ({initialCount}) => {
+  const [count, setCount] = useState(initialCount);
 
   const increment = () => {
     setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count -1);
+  }
+
+  const reset = () => {
+    setCount(initialCount);
   };
 
   return (
     <div>
       <p>The current count is {count}.</p>
       <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
 
+App.defaultProps = {
+  initialCount: 0,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App initialCount={10}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
