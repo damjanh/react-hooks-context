6437ad3cd76d8a9f832b43131abb92b8b5af99ec
damjanh <damjan.hudobreznik@gmail.com>
2020-06-29 11:14:29 +0200

    Use multiple variables with useState.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-const App = ({initialCount}) => {
+const App = ({initialCount, initialText}) => {
   const [count, setCount] = useState(initialCount);
+  const [text, setText] = useState(initialText);
 
   const increment = () => {
     setCount(count + 1);
@@ -19,16 +20,18 @@ const App = ({initialCount}) => {
 
   return (
     <div>
-      <p>The current count is {count}.</p>
+      <p>The current {text} is {count}.</p>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
       <button onClick={reset}>Reset</button>
+      <input type="text" value={text} onChange={(e)=> setText(e.target.value)}></input>
     </div>
   );
 }
 
 App.defaultProps = {
   initialCount: 0,
+  initialText: 'count',
 };
 
 ReactDOM.render(
