96280f0e6d86577a962876c73542cf0e4a294702
damjanh <damjan.hudobreznik@gmail.com>
2020-06-29 10:58:33 +0200

    Implement useState hook.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <p>The current count is {count}.</p>
+      <button onClick={increment}>+1</button>
+    </div>
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <div>My content</div>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
